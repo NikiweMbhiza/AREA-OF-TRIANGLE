a5293a2b0054028b881c058b1f2e2e91305260e8
Nikiw <Nikiw@NIKIWE_MBHIZA>
2024-01-17 10:01:28 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim A As Double
+        Dim H As Double
+        Dim L As Double
+        Dim P As Double
+        L = Val(TextBox1.Text)
+        H = Val(TextBox4.Text)
+        A = L * H
+        H = 2 * (L + H)
+        TextBox3.Text = A
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim A As Double
+        Dim H As Double
+        Dim L As Double
+        Dim P As Double
+        L = Val(TextBox1.Text)
+        H = Val(TextBox4.Text)
+        A = L * H
+        H = 2 * (L + H)
+        TextBox2.Text = H
+    End Sub
+End Class
